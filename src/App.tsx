668323b91f26ac88dc0e0f1620021e129627e254
Michał Szeniak <michal.szeniak@gmail.com>
2022-12-09 11:23:33 +0100

    BrowserRouter test

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ import TestPage from "./components/pages/TestPage/TestPage";
 function App() {
   return (
     <Routes>
-      <Route path="/results/:examId/:city" element={<TestPage />} />
-      <Route path="/test" element={<CustomDropdown />} />
-      <Route path="/raports" element={<RaportsPage />} />
+      <Route path="results/:examId/:city" element={<TestPage />} />
+      <Route path="test" element={<CustomDropdown />} />
+      <Route path="raports" element={<RaportsPage />} />
+      <Route path="*" element={<p>There's nothing here: 404! TEST</p>} />
     </Routes>
   );
 }
