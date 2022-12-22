1cd0acbbcc207365118aeb72fd4f166f25bd5bd2
MPasek <malzusiek666@gmail.com>
2022-12-22 14:33:42 +0100

    DetailsHeader with embedded google map

---
+++ src/components/organisms/DetailsHeader/DetailsHeader.tsx
@@ -0,0 +1,74 @@
+import Container from "react-bootstrap/Container";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+
+import "./DetailsHeader.scss";
+
+import StarsRating from "../../atoms/StarsRating";
+
+interface DetailsHeaderProps {
+  rating?: number;
+  facility: FacilityDataTypes["facility"];
+  scrollClick: () => void;
+}
+
+const DetailsHeader = ({
+  facility,
+  rating,
+  scrollClick = () => {}
+}: DetailsHeaderProps) => {
+  return (
+    <Container className="d-flex flex-column gap-5 p-0 align-items-center mb-5">
+      <div className="w-100 col-12">
+        <div className="google-map-container grid-container">
+          <Container
+            style={{ zIndex: "10002" }}
+            className="d-flex flex-column grid-item gap-5 p-0 py-3"
+          >
+            <h1 className="m-0 fw-bold results-title">{facility.name}</h1>
+            <p className="p-0">
+              {`ul. ${facility.street}, ${facility.zipCode} ${facility.city?.name}`}
+            </p>
+            <Container className="d-flex flex-column justify-content-center align-items-start px-0 m-0 gap-5">
+              <Container className="d-flex justify-content-start align-items-center p-0 gap-3">
+                {<StarsRating rating={rating} />}
+                <h4 className="m-0">
+                  <Badge bg="info" className="m-0">
+                    {rating ? `${rating}/5` : "N/A"}
+                  </Badge>
+                </h4>
+              </Container>
+              <Button className="btn-outline-pink" onClick={scrollClick}>
+                ZOBACZ OPINIE
+              </Button>
+            </Container>
+          </Container>
+          <iframe
+            className="grid-item"
+            title="Alivia"
+            id="placowka_mapa"
+            width="100%"
+            height="100%"
+            style={{
+              border: "0",
+              borderRadius: "0px 48px 48px 0px",
+              zIndex: "10000"
+            }}
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCnQEweM-kYWe0sHmlryq_IViRQMUZuwDU
+									&q=Alivia+Fundacja+onkologiczna+Warszawa"
+          ></iframe>
+
+          <div
+            style={{ zIndex: "10001" }}
+            className="google-map-cover grid-item"
+          ></div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default DetailsHeader;
--- src/components/templates/FacilityDetailsTemplate/FacilityDetailsTemplate.tsx
+++ src/components/templates/FacilityDetailsTemplate/FacilityDetailsTemplate.tsx
@@ -2,13 +2,11 @@ import { useRef } from "react";
 import { Container } from "react-bootstrap";
 import { useParams, useSearchParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import Button from "react-bootstrap/Button";
-import Badge from "react-bootstrap/Badge";
 
 import "./FacilityDetailsTemplate.scss";
 
 import { getFacilityDetails } from "../../../services/api/facilityDetailsApi";
-import StarsRating from "../../atoms/StarsRating";
+import DetailsHeader from "../../organisms/DetailsHeader";
 import ResultFilters from "../../molecules/ResultFilters";
 import ServiceDetails from "../../organisms/ServiceDetails";
 import FacilityReviews from "../../organisms/FacilityReviews";
@@ -47,38 +45,15 @@ const FacilityDetailsTemplate = () => {
 
   return (
     <Container className="d-flex flex-column p-5 gap-5 justify-content-center align-items-center">
-      <Container className="d-flex flex-column gap-5 p-0 align-items-center mb-5">
-        <h1 className="fw-bold results-title text-center mb-5">
-          {data && data.facility.name}
-        </h1>
-        <Container className="d-flex justify-content-center align-items-end border-top">
-          <Container className="d-flex flex-column justify-content-center align-items-center py-4 px-0 m-0 gap-1 border-end">
-            <p>
-              {data &&
-                `ul. ${data.facility.street}, ${data.facility.zipCode} ${data.facility.city?.name}`}
-            </p>
-            <Button className="btn-outline-pink">POKAŻ NA MAPIE</Button>
-          </Container>
-          <Container className="d-flex flex-column justify-content-center align-items-center py-4 px-0 m-0 gap-2">
-            <Container className="d-flex justify-content-center align-items-center p-0 gap-2">
-              {data && <StarsRating rating={data.rating} />}
-              <h4 className="m-0">
-                <Badge bg="info" className="m-0">
-                  {data?.rating ? `${data?.rating}/5` : "N/A"}
-                </Badge>
-              </h4>
-            </Container>
-            <Button
-              className="btn-outline-pink"
-              onClick={() =>
-                reviewsRef.current?.scrollIntoView({ behavior: "smooth" })
-              }
-            >
-              ZOBACZ OPINIE
-            </Button>
-          </Container>
-        </Container>
-      </Container>
+      {data && (
+        <DetailsHeader
+          facility={data.facility}
+          scrollClick={() =>
+            reviewsRef.current?.scrollIntoView({ behavior: "smooth" })
+          }
+          rating={data.rating}
+        />
+      )}
       <Container className="p-0 d-flex flex-column align-items-start gap-2 pb-3">
         <p className="fw-bold-600">Lista udogodnień:</p>
         <Container className="p-0 d-flex align-items-center gap-3">
@@ -94,7 +69,7 @@ const FacilityDetailsTemplate = () => {
         <Container className="d-flex justify-content-between align-items-baseline p-0">
           <h2 className="fw-bold">Terminarz</h2>
           <p>
-            Ostatnia aktualizacja:{" "}
+            Ostatnia aktualizacja:
             <span className="fw-bold">6 września 2022</span>
           </p>
         </Container>
@@ -112,8 +87,10 @@ const FacilityDetailsTemplate = () => {
             />
           ))}
       </Container>
-      <div ref={reviewsRef} className="w-100 border-top pb-5"></div>
-      <FacilityReviews rating={data?.rating} />
+      <div className="w-100 border-top pb-5"></div>
+      <div ref={reviewsRef} className="p-0 m-0">
+        <FacilityReviews rating={data?.rating} />
+      </div>
     </Container>
   );
 };
