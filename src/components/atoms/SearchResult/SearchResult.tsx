a5c5323a44a384e0e84914e0d458d63b9f319f66
MPasek <malzusiek666@gmail.com>
2022-12-28 17:24:50 +0100

    RWD, data & pagination changes

--- src/components/atoms/SearchResult/SearchResult.tsx
+++ src/components/atoms/SearchResult/SearchResult.tsx
@@ -10,9 +10,17 @@ import PercentageProgress from "../PercentageProgress";
 
 interface SearchResultProps {
   facility: FacilityDataTypes;
+  avgSuccessfulCallsPercents?: number;
+  ratingCount?: number;
+  latestSurveys?: LatestSurveyTypes[];
 }
 
-const SearchResult = ({ facility }: SearchResultProps) => {
+const SearchResult = ({
+  facility,
+  avgSuccessfulCallsPercents,
+  ratingCount,
+  latestSurveys
+}: SearchResultProps) => {
   return (
     <Container className="d-flex flex-column fc-main border rounded-1 p-0">
       <Container className="row m-0 p-0 result-top-row-bg">
@@ -30,22 +38,37 @@ const SearchResult = ({ facility }: SearchResultProps) => {
         <Container className="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center border-end border-bottom py-4">
           <p>Najbliższa wizyta za</p>
           <h4>
-            <Badge bg="info">{`10 DNI`}</Badge>
+            <Badge bg="info">
+              {" "}
+              {latestSurveys
+                ? latestSurveys[0].daysToResults
+                  ? `${latestSurveys[0].daysToResults} DNI`
+                  : "brak danych"
+                : "brak danych"}
+            </Badge>
           </h4>
         </Container>
 
         <Container className="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center py-4">
           <p>Oczekiwanie na opis badania</p>
           <h4>
-            <Badge bg="info">{`10 DNI`}</Badge>
+            <Badge bg="info">
+              {latestSurveys
+                ? latestSurveys[0].daysUntilExamination
+                  ? `${latestSurveys[0].daysUntilExamination} DNI`
+                  : "brak danych"
+                : "brak danych"}
+            </Badge>
           </h4>
         </Container>
       </Container>
 
       <Container className="row m-0 p-0 border-top">
-        <Container className="col-12 col-md-3 d-flex flex-column align-items-start border-end border-bottom gap-2 pt-4 pb-4 ps-4 pe-5">
+        <Container className="col-12 col-md-3 d-flex flex-column align-items-start border-end border-bottom gap-2 pt-4 pb-4 px-4">
           <p className="m-0">{`ul. ${facility.facility.street}, ${facility.facility.zipCode} ${facility.facility.city?.name}`}</p>
-          <Button className="btn-outline-pink">POKAŻ NA MAPIE</Button>
+          <Button className="btn-outline-pink results-rwd-button">
+            POKAŻ NA MAPIE
+          </Button>
         </Container>
 
         <Container className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center border-end border-bottom gap-3 py-3">
@@ -53,24 +76,35 @@ const SearchResult = ({ facility }: SearchResultProps) => {
           <Container className="d-flex flex-row justify-content-center align-items-center gap-2">
             <h4 className="m-0">
               <Badge bg="info" className="m-0">
-                {facility.rating ? `${facility.rating.toFixed(1)}/5` : "N/A"}
+                {facility.rating ? `${facility.rating.toFixed(1)}/5` : "-/5"}
               </Badge>
             </h4>
             <p className="m-0 align-self-center fs-13">{`(${
-              facility.totalCalls ? facility.totalCalls : "brak"
+              ratingCount ? ratingCount : "-"
             } opinii)`}</p>
           </Container>
         </Container>
 
         <Container className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center border-end border-bottom py-3 gap-2">
-          <PercentageProgress percentage={facility.avgTotalCallsPercents} />
+          <PercentageProgress
+            percentage={
+              avgSuccessfulCallsPercents
+                ? Math.floor(avgSuccessfulCallsPercents)
+                : 0
+            }
+          />
           <p className="text-center m-0 fs-13">
             udanych połączeń telefonicznych do placówki
           </p>
         </Container>
 
-        <Container className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center py-3">
-          <Button className="btn-pink">UMÓW SIĘ</Button>
+        <Container className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center p-4">
+          <Link
+            className="results-rwd-button"
+            to={`/details/${facility.facility.id}`}
+          >
+            <Button className="btn-pink results-rwd-button">UMÓW SIĘ</Button>
+          </Link>
         </Container>
       </Container>
     </Container>
